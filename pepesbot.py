3e400ff370c21f0e676536bdf70a9c6fdd7d2716
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-11 23:39:15 +0100

    Create pepesbot.py

---
+++ pepesbot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import platform
+import colorsys
+import random
+import os
+import time
+from discord.voice_client import VoiceClient
+from discord import Game, Embed, Color, Status, ChannelType
+import youtube_dl
+
+
+
+
+
+
+
+
+bot=commands.Bot(command_prefix='?')
